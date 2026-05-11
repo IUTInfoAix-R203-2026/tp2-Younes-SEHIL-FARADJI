1da17a3d2270d0fce798e3911948b909e852e857
Younes SEHIL FARADJI <younes.SEHIL-FARADJI@etu.univ-amu.fr>
2026-05-11 09:20:27 +0000

    Exercice 2 terminé

--- src/test/java/fr/univ_amu/iut/exercice2/FizzBuzzerTest.java
+++ src/test/java/fr/univ_amu/iut/exercice2/FizzBuzzerTest.java
@@ -28,19 +28,16 @@ class FizzBuzzerTest {
     fizzBuzz = new FizzBuzzer();
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_de_1_retourne_1() {
     assertThat(fizzBuzz.fizzBuzz(1)).isEqualTo("1");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_de_2_retourne_2() {
     assertThat(fizzBuzz.fizzBuzz(2)).isEqualTo("2");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_de_3_retourne_fizz() {
     assertThat(fizzBuzz.fizzBuzz(3)).isEqualTo("Fizz");
@@ -52,7 +49,6 @@ class FizzBuzzerTest {
     assertThat(fizzBuzz.fizzBuzz(4)).isEqualTo("4");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_de_5_retourne_buzz() {
     assertThat(fizzBuzz.fizzBuzz(5)).isEqualTo("Buzz");
@@ -76,25 +72,21 @@ class FizzBuzzerTest {
     assertThat(fizzBuzz.fizzBuzz(10)).isEqualTo("Buzz");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_de_15_retourne_fizz_buzz() {
     assertThat(fizzBuzz.fizzBuzz(15)).isEqualTo("FizzBuzz");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_jusqu_a_5_retourne_un_tableau_de_5_elements() {
     assertThat(fizzBuzz.fizzBuzzJusquA(5)).hasSize(5);
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_jusqu_a_5_retourne_1_2_fizz_4_buzz() {
     assertThat(fizzBuzz.fizzBuzzJusquA(5)).containsExactly("1", "2", "Fizz", "4", "Buzz");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void fizz_buzz_jusqu_a_15_correspond_aux_appels_individuels() {
     String[] sequence = fizzBuzz.fizzBuzzJusquA(15);
